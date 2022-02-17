daa0c925688d4a9ffae26304ca551e01be478ed9
yakorch <92575094+yakorch@users.noreply.github.com>
2022-02-17 20:51:52 +0200

    Create flask_app.py

---
+++ flask_app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+import createweb
+app = Flask(__name__)
+
+#@app.route('/')
+#def hello_world():
+#    return 'Hello from Flask!'
+
+@app.route('/')
+def map_func():
+ return render_template("index.html")
+
+@app.route("/redirecting", methods=["POST"])
+def redirecting():
+    if request.method == "POST":
+        dct = request.form
+        nickname = dct["name"]
+        createweb.main(nickname)
+        return render_template("twitter_friends.html")
